15746420f95bb2b538cd2ef1ccd51a76afa78922
Jesse Meirink <jesse@meirink.com>
2022-08-06 14:45:59 -0500

    complete drum edit and delete functionality

--- main_app/urls.py
+++ main_app/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
   path('about/', views.about, name='about'),
   path('drums/', views.drums_index, name='drums_index'),
   path('drums/<int:drum_id>/', views.drums_detail, name='drums_detail'),
+  path('drums/create/', views.DrumCreate.as_view(), name='drums_create'),
+  path('drums/<int:pk>/update/', views.DrumUpdate.as_view(), name='drums_update'),
+  path('drums/<int:pk>/delete/', views.DrumDelete.as_view(), name='drums_delete'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Drum
 
 # Create your views here.
@@ -15,3 +16,17 @@ def drums_index(request):
 def drums_detail(request, drum_id):
   drum = Drum.objects.get(id=drum_id)
   return render(request, 'drums/detail.html', { 'drum': drum })
+
+class DrumCreate(CreateView):
+  model = Drum
+  fields = '__all__'
+  success_url = '/drums/'
+
+class DrumUpdate(UpdateView):
+  model = Drum
+  # Let's disallow the renaming of a cat by excluding the name field!
+  fields = ['type', 'description']
+
+class DrumDelete(DeleteView):
+  model = Drum
+  success_url = '/drums/'
